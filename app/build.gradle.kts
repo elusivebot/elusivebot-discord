01bf576b5e54871e63a3bd241d3df9ce0e97f439
Bryan DeGrendel <bryan@degrendel.com>
2023-05-07 15:20:31 -0400

    Add Kord to dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 }
 
 dependencies {
+    implementation("dev.kord:kord-core:0.9.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
 }
