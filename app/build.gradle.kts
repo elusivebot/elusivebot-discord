e6d046fab4b6f34da1da54bb57123fa7cf4cf560
Bryan DeGrendel <bryan@degrendel.com>
2024-03-12 17:51:52 -0400

    Update Gradle model
    Latest versions of things plus linting.  Change main placeholder to make DikTat happy.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,37 +2,66 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.8.21"
     application
     id("net.researchgate.release") version "3.0.2"
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
-repositories {
-  mavenCentral()
-}
-
-dependencies {
-    implementation("dev.kord:kord-core:0.9.0")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
-}
-
-application {
-  mainClass.set("com.sirnuke.elusivebot.discord.MainKt")
-}
-
-tasks.named<Test>("test") {
-  useJUnitPlatform()
-}
-
-tasks.register("printVersion") {
-  doLast {
-    println(project.version)
-  }
-}
-
-configure<net.researchgate.release.ReleaseExtension> {
-  tagTemplate.set("v\${version}")
-    with(git) {
-      requireBranch.set("main")
+spotless {
+    kotlin {
+        diktat()
+    }
+    kotlinGradle {
+        diktat()
     }
 }
 
-group = "com.sirnuke"
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+group = "com.sirnuke.elusivebot"
+
+val kafkaApiVersion = "3.6.1"
+
+dependencies {
+    implementation("org.slf4j:slf4j-simple:2.0.12")
+    implementation("com.uchuhimo:konf:1.1.2")
+    implementation("dev.kord:kord-core:0.13.1")
+    implementation("org.apache.kafka:kafka-streams:$kafkaApiVersion")
+    implementation("org.apache.kafka:kafka-clients:$kafkaApiVersion")
+
+    implementation("com.sirnuke.elusivebot:elusivebot-schema:0.1.0-SNAPSHOT")
+    implementation("com.sirnuke.elusivebot:elusivebot-common:0.1.0-SNAPSHOT")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    testImplementation("org.apache.kafka:kafka-streams-test-utils:$kafkaApiVersion")
+}
+
+application {
+    mainClass.set("com.sirnuke.elusivebot.discord.MainKt")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.register("printVersion") {
+    doLast {
+        @Suppress("DEBUG_PRINT")
+        println(project.version)
+    }
+}
+
+configure<net.researchgate.release.ReleaseExtension> {
+    tagTemplate.set("v\${version}")
+    with(git) {
+        requireBranch.set("main")
+    }
+}
--- app/src/main/kotlin/com/sirnuke/elusivebot/discord/Main.kt
+++ app/src/main/kotlin/com/sirnuke/elusivebot/discord/Main.kt
@@ -1,4 +1,12 @@
+/**
+ * Entrypoint for ElusiveBot's Discord interface.
+ */
+
 package com.sirnuke.elusivebot.discord
 
-suspend fun main() {
+import org.slf4j.LoggerFactory
+
+fun main() {
+    val log = LoggerFactory.getLogger("com.sirnuke.elusivebot.discord.MainKt")
+    log.info("Hello world!")
 }
