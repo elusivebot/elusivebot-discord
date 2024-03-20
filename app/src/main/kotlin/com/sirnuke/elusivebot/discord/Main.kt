ed86153a3db9b90088bbfa4013ba43647b149e30
Bryan DeGrendel <bryan@degrendel.com>
2024-03-20 18:43:30 -0400

    Implement parsing JSON messages
    Aka should be able to send and receive messages on Kafka now, and hopefully ready for an initial deployment.

--- app/src/main/kotlin/com/sirnuke/elusivebot/discord/Main.kt
+++ app/src/main/kotlin/com/sirnuke/elusivebot/discord/Main.kt
@@ -4,13 +4,19 @@
 
 package com.sirnuke.elusivebot.discord
 
+import com.sirnuke.elusivebot.schema.common.Header
+import com.sirnuke.elusivebot.schema.messages.ChatMessage
 import com.uchuhimo.konf.Config
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
+import dev.kord.core.entity.channel.MessageChannel
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
 import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
@@ -18,11 +24,14 @@ import org.apache.kafka.streams.StreamsConfig
 import org.apache.kafka.streams.kstream.KStream
 import org.slf4j.LoggerFactory
 
+import java.lang.Exception
 import java.util.concurrent.atomic.AtomicBoolean
 
 import kotlin.concurrent.thread
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @Suppress("TOO_LONG_FUNCTION")
 fun main() = runBlocking {
@@ -54,24 +63,58 @@ fun main() = runBlocking {
 
     val consumer: KStream<String, String> = builder.stream(config[DiscordSpec.Kafka.consumerChannel])
 
+    val kord = Kord(config[DiscordSpec.discordToken])
+
     consumer.foreach { key, raw ->
         log.info("Got input {} {}", key, raw)
+        val chatMessage: ChatMessage = Json.decodeFromString(raw)
         if (running.get()) {
-            // TODO: Pass messages to Kord
+            this.launch {
+                val channelId = chatMessage.header.channelId?.toULong()
+                channelId ?: run {
+                    log.warn("Received response message without a channelId! {}: {}", key, raw)
+                    return@launch
+                }
+                val channel: MessageChannel? = kord.getChannelOf(Snowflake(channelId))
+                channel ?: run {
+                    log.warn("Unable to find channel with id {}: {}: {}", channelId, key, raw)
+                    return@launch
+                }
+                channel.createMessage(content = chatMessage.message)
+            }
         }
     }
 
     val streams = KafkaStreams(builder.build(), consumerConfig)
     streams.start()
 
-    val kord = Kord(config[DiscordSpec.discordToken])
-
     kord.on<MessageCreateEvent> {
         // No bots, and cancel if in the process of shutting down the service
         if (!running.get() || message.author?.isBot != false) {
             return@on
         }
-        // TODO: Process and pass message to Kafka
+        // TODO: Fairly certain guildId is null if it's a DM, may want a batter way of handling this
+        val guildId = this.guildId?.toString() ?: "direct-message"
+        val channelId = message.channelId.toString()
+        val chatMessage = ChatMessage(
+            header = Header(
+                serviceId = config[DiscordSpec.serviceId],
+                serverId = guildId,
+                channelId = channelId,
+            ),
+            user = message.author?.id.toString(),
+            message = message.content,
+        )
+        producer.send(
+            ProducerRecord(
+                config[DiscordSpec.Kafka.producerChannel], channelId, Json.encodeToString(chatMessage)
+            )
+        ) { _: RecordMetadata?, ex: Exception? ->
+            // TODO: Better logging
+            ex?.let {
+                log.error("Unable to forward Discord response", ex)
+            } ?: log.info("Done forwarding Discord message")
+        }
     }
 
     Runtime.getRuntime().addShutdownHook(thread(start = false, name = "shutdown-hook") {
